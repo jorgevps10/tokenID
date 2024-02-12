8461ec93c36b60dd7a7df8f0fba98424c19031f6
jorgevps10 <gitinit4@gmail.com>
2024-02-11 22:45:08 -0300

    se agrega el proyecto

--- tokenID.sh
+++ tokenID.sh
@@ -17,22 +17,22 @@ bar1="\e[1;30m◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚
 bar2="\033[38;5;226m---------------------------------------------------------\033[0m"
 bar3="\033[38;5;226m--------------------- = MENU = --------------------------\033[0m"
 #PATH DE ARCHVOS
-dir_token=/etc/token
-file_token=$dir_token/BD
-file_pass=$dir_token/.passw
-file_client=$dir_token/.clientes
+dir_hwid=/etc/hwid
+file_hwid=$dir_hwid/BD
+file_pass=$dir_hwid/.passw
+file_client=$dir_hwid/.clientes
 #DIRECTORIOS
 mkdir_dir (){
-  if [ -d $dir_token ]; then
+  if [ -d $dir_hwid ]; then
 	    echo
 	    else
-	     mkdir $dir_token
+	     mkdir $dir_hwid
 fi
-if [ -f $file_token ]; then
+if [ -f $file_hwid ]; then
 	    echo
 	    else
-	     touch $file_token
-       chmod 755 $file_token
+	     touch $file_hwid
+       chmod 755 $file_hwid
 fi
 if [ -f $file_pass ]; then
 	    echo
@@ -56,7 +56,7 @@ msg () {
 echo -e "$bar1"
     echo -e "${rojo}########################################################${cierre}"
     echo -e "${rojo}#    ${verde}Bienvenido${blanco}, Gracias por utilizar Onlycode ${verdeR}VPN     ${rojo}# ${cierre}"
-    echo -e "${rojo}#    ${blanco}Este SCRIPT autoriza su token ID para usar${cierre}        ${rojo}# ${cierre}"
+    echo -e "${rojo}#    ${blanco}Este SCRIPT autoriza su hwid ID para usar${cierre}        ${rojo}# ${cierre}"
     echo -e "${rojo}#    ${blanco}Nuestra apk...                            ${cierre}        ${rojo}# ${cierre}"
     echo -e "${rojo}#   ${melon}-----------------------------------------------    ${rojo}#${cierre}"
     echo -e "${rojo}#          ${blanco}dev: ${moradoL}@dankelthaher ${blanco}- ${amarillo}Onlycode ${verdeR}VPN${cierre}           ${rojo}#${cierre}"
@@ -79,25 +79,25 @@ crear () {
     TITLE="${blanco}Onlycode ${verdeR}VPN"
 echo -e "$TITLE"
 echo -e ""
-echo -e " AUTORIZAR TOKEN ID"
+echo -e " AUTORIZAR hwid ID"
 echo -e "$bar1"
 echo -e ""
 echo -e "${blanco}Ingrese el Nombre del Usuario${cierre}"
 echo -e "$bar2"
 read -p $'\e[32m  [ingrese]: \e[0m' usr
 echo -e "$bar1"
-echo -e "${blanco}Ingrese el Token que desea Autorizar${cierre}"
+echo -e "${blanco}Ingrese el hwid que desea Autorizar${cierre}"
 echo -e "$bar2"
-read -p $'\e[32m  [Token ID]: \e[0m' token
+read -p $'\e[32m  [hwid ID]: \e[0m' hwid
 
 # Check If Username Exist, Else Proceed
-egrep "^$token" /etc/passwd >/dev/null
+egrep "^$hwid" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 clear
 echo -e "$bar1"
 echo -e "$TITLE"
 echo ""
-echo -e "\e[31m El Token ya existe en su servidor, intente con otro Token\e[0m."
+echo -e "\e[31m El hwid ya existe en su servidor, intente con otro hwid\e[0m."
 exit 0
 else
 echo -e "$bar2"
@@ -117,20 +117,20 @@ stunnel4port="$(netstat -nlpt | grep -i stunnel4 | grep -i 0.0.0.0 | awk '{print
 openvpnport="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2 | xargs | sed -e 's/ /, /g')"
 pyport="$(netstat -nlpt | grep -i python | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2 | xargs | sed -e 's/ /, /g')"
 squidport="$(cat /etc/squid/squid.conf | grep -i http_port | awk '{print $2}' | xargs | sed -e 's/ /, /g')"
-useradd -m -s /bin/false -e $Expiration $token > /dev/null
-egrep "^$token" /etc/passwd &> /dev/null
+useradd -m -s /bin/false -e $Expiration $hwid > /dev/null
+egrep "^$hwid" /etc/passwd &> /dev/null
 DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
-echo -e "$(cat $file_pass)\n$(cat $file_pass)\n" | passwd $token &> /dev/null
-echo -e "$token" >> $file_token
-echo -e "$usr  $token  $DIA     $HORA"  >> $file_client
+echo -e "$(cat $file_pass)\n$(cat $file_pass)\n" | passwd $hwid &> /dev/null
+echo -e "$hwid" >> $file_hwid
+echo -e "$usr  $hwid  $DIA     $HORA"  >> $file_client
 clear
 echo -e "$bar1"
 echo -e "$TITLE"
 echo -e "$bar2"
 echo ""
 echo -e " Datos del Usuario:"
-echo -e "\e[32m  Token ID: \e[0m"$token
+echo -e "\e[32m  hwid ID: \e[0m"$hwid
 echo -e "\e[32m  Caducidad de la cuenta: \e[0m"$Expiration_Display
 echo -e ""
 echo -e "$bar1"
@@ -148,11 +148,11 @@ menu
 }
 #ELIMINAR ID
 eliminar () {
-  unset token
+  unset hwid
   echo -e "$TITLE"
 echo -e ""
     i=1
-	for total in `cat $file_token`; do
+	for total in `cat $file_hwid`; do
 	[[ -f "$total" ]] && continue
 	option_id[$i]="$total"
     echo -e "$bar1"
@@ -165,41 +165,41 @@ echo -e ""
 read -p "seleccione [1-$i]: " slct_option
 tput cuu1 && tput dl1
 done
-token="${option_id[$slct_option]}"
+hwid="${option_id[$slct_option]}"
 sleep 2
-egrep "^$token" /etc/passwd &> /dev/null
+egrep "^$hwid" /etc/passwd &> /dev/null
 if [ $? -eq 0 ]; then
-  userdel -f $token
-  rm -rf /home/$token
-  sed -i "s;$token;;g" $file_token
-  sed -i '/^$/d' $file_token
-  sed -i "/$token/d" $file_client
+  userdel -f $hwid
+  rm -rf /home/$hwid
+  sed -i "s;$hwid;;g" $file_hwid
+  sed -i '/^$/d' $file_hwid
+  sed -i "/$hwid/d" $file_client
   sed -i '/^$/d' $file_client
 	clear
 	echo -e ""
   echo -e "$TITLE"
   echo -e "$bar1"
-	echo -e " Token Eliminado con Exito"
+	echo -e " hwid Eliminado con Exito"
 	echo -e "$bar1"
 else
 	clear
 	echo -e ""
   echo -e "$TITLE"
   echo -e "$bar1"
-	echo -e " El Token que ingresaste no existe"
+	echo -e " El hwid que ingresaste no existe"
 	echo -e "$bar1"
   msg -ne "Enter Para Continuar" && read enter
 menu
 fi
 
 }
-#EDITAR TOKEN
+#EDITAR hwid
 editar (){
-unset token
+unset hwid
   echo -e "$TITLE"
 echo -e ""
     i=1
-	for total in `cat $file_token`; do
+	for total in `cat $file_hwid`; do
 	[[ -f "$total" ]] && continue
 	option_id[$i]="$total"
     echo -e "$bar1"
@@ -212,9 +212,9 @@ echo -e ""
 read -p "seleccione [1-$i]: " slct_option
 tput cuu1 && tput dl1
 done
-token="${option_id[$slct_option]}"
+hwid="${option_id[$slct_option]}"
 sleep 2
-egrep "^$token" /etc/passwd >/dev/null
+egrep "^$hwid" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 echo -e "${blanco}Ingrese el numero de dias${cierre}"
 echo -e "$bar2"
@@ -224,15 +224,15 @@ Days_Detailed=$(( $Days * 86400 ))
 Expire_On=$(($Today + $Days_Detailed))
 Expiration=$(date -u --date="1970-01-01 $Expire_On sec GMT" +%Y/%m/%d)
 Expiration_Display=$(date -u --date="1970-01-01 $Expire_On sec GMT" '+%d %b %Y')
-passwd -u $token
-usermod -e $Expiration $token
-egrep "^$token" /etc/passwd >/dev/null
-echo -e "$PASSWDT\n$PASSWDT\n"|passwd $token &> /dev/null
+passwd -u $hwid
+usermod -e $Expiration $hwid
+egrep "^$hwid" /etc/passwd >/dev/null
+echo -e "$PASSWDT\n$PASSWDT\n"|passwd $hwid &> /dev/null
 clear
 echo -e "$TILTE"
 echo -e "$bar1"
 echo -e ""
-echo -e " token ID: $token"
+echo -e " hwid ID: $hwid"
 echo -e "$bar2"
 echo -e " Caducidad de cuenta modificada: $Days Dias"
 echo -e "$bar2"
@@ -243,7 +243,7 @@ clear
 echo -e "$TILTE"
 echo -e "$bar1"
 echo -e ""
-echo -e " El Token que ingresaste no existe"
+echo -e " El hwid que ingresaste no existe"
 echo -e ""
 fi
 msg -ne "Enter Para Continuar" && read enter
@@ -264,7 +264,7 @@ clear
 echo -e "$TILTE"
 echo -e "$bar1"
 echo -e ""
-echo -e " Token ID               Expiracion"
+echo -e " hwid ID               Expiracion"
 echo -e "$bar1"
 while read Checklist
 do
@@ -278,7 +278,7 @@ do
 done < /etc/passwd
 No_Users="$(awk -F: '$3 >= '$UIDN' && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo -e "$bar2"
-echo -e " Numero de Tokens: "$No_Users
+echo -e " Numero de hwids: "$No_Users
 echo -e "$bar2"
 echo -e "Enter Para Continuar" && read enter
 menu
@@ -286,23 +286,23 @@ menu
 #ELIMINAR EXPIRADOS
 expirados () {
 clear
-unset token
+unset hwid
 echo -e ""
 echo -e "$TITLE"
 echo -e "$bar1"
 
 datenow=$(date +%s)
-for token in $(awk -F: '{print $1}' /etc/passwd); do
-expdate=$(chage -l $token|awk -F: '/Account expires/{print $2}')
+for hwid in $(awk -F: '{print $1}' /etc/passwd); do
+expdate=$(chage -l $hwid|awk -F: '/Account expires/{print $2}')
 echo $expdate | grep -q never && continue
-echo -n "Token \`$token' expira el $expdate ... "
+echo -n "hwid \`$hwid' expira el $expdate ... "
 expsec=$(date +%s --date="$expdate")
 diff=$(echo $datenow - $expsec|bc -l)
 echo $diff | grep -q ^\- && echo okay && continue
-userdel -r -f $token  &> /dev/null
+userdel -r -f $hwid  &> /dev/null
 done
 echo -e ""
-echo -e "  Todos \e[31mLos token expirados\e[0m han sido Eliminados"
+echo -e "  Todos \e[31mLos hwid expirados\e[0m han sido Eliminados"
 echo -e "$bar1"
 msg -ne "Enter Para Continuar" && read enter
 menu
@@ -340,7 +340,7 @@ do
         USER=`cat /tmp/login-db-pid.txt | awk '{print $10}'`
         IP=`cat /tmp/login-db-pid.txt | awk '{print $12}'`
         if [ $NUM -eq 1 ]; then
-                echo -e "Token: $USER - ${verdeR}[ON]${cierre} -"
+                echo -e "hwid: $USER - ${verdeR}[ON]${cierre} -"
                 fi
 done
 GreenBG="\033[42;31m"
@@ -358,7 +358,7 @@ do
         USER=`cat /tmp/login-db-pid.txt | awk '{print $9}'`;
         IP=`cat /tmp/login-db-pid.txt | awk '{print $11}'`;
         if [ $NUM -eq 1 ]; then
-                echo -e "Token: $USER - ${verdeR}[ON]${cierre} -";
+                echo -e "hwid: $USER - ${verdeR}[ON]${cierre} -";
         fi
 done
 if [ -f "/etc/openvpn/openvpn-status.log" ]; then
@@ -379,13 +379,13 @@ echo " "
 msg -bar "Enter Para Continuar" && read enter
 menu
 }
-No_token="$(cat /etc/token/BD | wc -l)"
+No_hwid="$(cat /etc/hwid/BD | wc -l)"
 #MENU DE USUARIOS
 menu () {
   clear
-echo -e "${melon}               == Token Auth == (${amarillo}onlycode${cierre}${melon})
+echo -e "${melon}               == hwid Auth == (${amarillo}onlycode${cierre}${melon})
 ${bar1}
-${morado}TOTAL DE REGISTROS: ${rojo}>${cierre} ${azul}ID: ${blanco}$No_token  ${cierre}
+${morado}TOTAL DE REGISTROS: ${rojo}>${cierre} ${azul}ID: ${blanco}$No_hwid  ${cierre}
 ${bar3}
 ${azul}[1]${cierre} ${rojo}>${cierre} ${blanco}Agregar ${verdeR}ID${cierre}
 ${azul}[2]${cierre} ${rojo}>${cierre} ${blanco}Eliminar ${rojo}ID${cierre}
@@ -393,7 +393,7 @@ ${azul}[3]${cierre} ${rojo}>${cierre} ${blanco}Editar Expiracion ${verde}ID${cie
 ${azul}[4]${cierre} ${rojo}>${cierre} ${blanco}Lista de ${verde}ID${cierre}
 ${azul}[5]${cierre} ${rojo}>${cierre} ${blanco}Eliminar ${verde}ID ${blanco}Expirados${cierre}
 ${azul}[6]${cierre} ${rojo}>${cierre} ${blanco}Informacion de Clientes${cierre}
-${azul}[7]${cierre} ${rojo}>${cierre} ${blanco}Token ${verdeR}Activos${cierre}
+${azul}[7]${cierre} ${rojo}>${cierre} ${blanco}hwid ${verdeR}Activos${cierre}
 ${azul}[0]${cierre} ${rojo}>${cierre} ${rojo}SALIR${cierre}
 ${bar2}"
 read -p "$(echo -e "${blanco}seleccione [0-7]:${cierre}")" selection
